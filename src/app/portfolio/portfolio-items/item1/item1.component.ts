43e11f60b2613bd2d87c554791b8b742df5a542a
Stefan <shodgeskluck@gmail.com>
2019-06-09 21:00:26 -0400

    Fixed portfolio sub-level routing

--- src/app/portfolio/portfolio-items/item1/item1.component.ts
+++ src/app/portfolio/portfolio-items/item1/item1.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-item1',
   templateUrl: './item1.component.html',
   styleUrls: ['./item1.component.scss']
 })
--- src/app/portfolio/portfolio-items/item2/item2.component.ts
+++ src/app/portfolio/portfolio-items/item2/item2.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-item2',
   templateUrl: './item2.component.html',
   styleUrls: ['./item2.component.scss']
 })
