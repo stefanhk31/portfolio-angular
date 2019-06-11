9c03254571430883517f184f3dfccb945387591f
Stefan <shodgeskluck@gmail.com>
2019-06-11 08:45:55 -0400

    Added modals to portfolio items

--- src/app/portfolio/portfolio-items/portfolio-items.component.ts
+++ src/app/portfolio/portfolio-items/portfolio-items.component.ts
@@ -14,14 +14,26 @@ export class PortfolioItemsComponent implements OnInit {
 
   items: any[] = [
     {
-      "itemID": 1,
+      "itemID": "whereToDo",
       "itemName": "WhereToDo",
-      "itemTagline": "A to-do list app that geolocates tasks."
+      "itemTagline": "A to-do list app that geolocates tasks.",
+      "itemDescription": {
+        "project": "A short overview of what it does and why I did it.",
+        "tech": "The frameworks I used and things I learned from the experience.",
+        "future": "What I'd like to do in the future and/or what I would've done differently."
+      },
+      "itemImage": "This will be an image URL."
     },
     {
-      "itemID": 2,
+      "itemID": "sitStandTimer",
       "itemName": "Sit/Stand Timer",
-      "itemTagline": "A timer app used to remind you to change position while working."
+      "itemTagline": "A timer app used to remind you to change position while working.",
+      "itemDescription": {
+        "project": "A short overview of what it does and why I did it.",
+        "tech": "The frameworks I used and things I learned from the experience.",
+        "future": "What I'd like to do in the future and/or what I would've done differently."
+      },
+      "itemImage": "This will be an image URL."
     }
   ]
 
