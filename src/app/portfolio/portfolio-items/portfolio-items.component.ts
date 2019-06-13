532ba2a968090b808f1b02bfc102bedd2f9e1a4d
Stefan <shodgeskluck@gmail.com>
2019-06-13 09:02:02 -0400

    Put portfolio items into service

--- src/app/portfolio/portfolio-items/portfolio-items.component.ts
+++ src/app/portfolio/portfolio-items/portfolio-items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IPortfolioItem } from './portfolioItem';
+import { PortfolioService } from './portfolio.service';
 
 @Component({
   selector: 'portfolio-items',
@@ -8,34 +9,12 @@ import { IPortfolioItem } from './portfolioItem';
 })
 export class PortfolioItemsComponent implements OnInit {
 
-  constructor() { }
+  items: IPortfolioItem[] = [];
 
-  ngOnInit() {
+  constructor(private portfolioService: PortfolioService) { }
+
+  ngOnInit(): void {
+    this.items = this.portfolioService.getPortfolioItems();
   }
-
-  items: IPortfolioItem[] = [
-    {
-      "itemID": "whereToDo",
-      "itemName": "WhereToDo",
-      "itemTagline": "A to-do list app that geolocates tasks.",
-      "itemDescription": {
-        "project": "A short overview of what it does and why I did it.",
-        "tech": "The frameworks I used and things I learned from the experience.",
-        "future": "What I'd like to do in the future and/or what I would've done differently."
-      },
-      "itemImage": "This will be an image URL."
-    },
-    {
-      "itemID": "sitStandTimer",
-      "itemName": "Sit/Stand Timer",
-      "itemTagline": "A timer app used to remind you to change position while working.",
-      "itemDescription": {
-        "project": "A short overview of what it does and why I did it.",
-        "tech": "The frameworks I used and things I learned from the experience.",
-        "future": "What I'd like to do in the future and/or what I would've done differently."
-      },
-      "itemImage": "This will be an image URL."
-    }
-  ]
 
 }
---
+++ src/app/portfolio/portfolio-items/portfolio.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { IPortfolioItem } from './portfolioItem';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PortfolioService {
+
+    getPortfolioItems(): IPortfolioItem[] {
+        return [
+            {
+              "itemID": "whereToDo",
+              "itemName": "WhereToDo",
+              "itemTagline": "A to-do list app that geolocates tasks.",
+              "itemDescription": {
+                "project": "A short overview of what it does and why I did it.",
+                "tech": "The frameworks I used and things I learned from the experience.",
+                "future": "What I'd like to do in the future and/or what I would've done differently."
+              },
+              "itemImage": "This will be an image URL."
+            },
+            {
+              "itemID": "sitStandTimer",
+              "itemName": "Sit/Stand Timer",
+              "itemTagline": "A timer app used to remind you to change position while working.",
+              "itemDescription": {
+                "project": "A short overview of what it does and why I did it.",
+                "tech": "The frameworks I used and things I learned from the experience.",
+                "future": "What I'd like to do in the future and/or what I would've done differently."
+              },
+              "itemImage": "This will be an image URL."
+            }
+          ];
+    }
+
+}
