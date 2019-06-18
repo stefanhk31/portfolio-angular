e3e5a248b74847cb5d8021ccbbff512d8a29ab86
Stefan <shodgeskluck@gmail.com>
2019-06-18 08:59:47 -0400

    Removed debugging code

--- src/app/portfolio/portfolio-items/portfolio.service.ts
+++ src/app/portfolio/portfolio-items/portfolio.service.ts
@@ -14,25 +14,9 @@ export class PortfolioService {
     constructor(private http: HttpClient) { }
 
     getPortfolioItems(): Observable<IPortfolioItem[]> {
-        return this.http.get<IPortfolioItem[]>(this.portfolioItemsSource).pipe(
-          tap(data => console.log('All: ' + JSON.stringify(data))),
-          catchError(this.handleError)
-          )
+        return this.http.get<IPortfolioItem[]>(this.portfolioItemsSource)
     }
 
-    private handleError(err: HttpErrorResponse) {
-      let errorMessage = '';
-      if (err.error instanceof ErrorEvent) {
-        // A client-side or network error occurred. Handle it accordingly.
-        errorMessage = `An error occurred: ${err.error.message}`;
-      } else {
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong,
-        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-      }
-      console.error(errorMessage);
-      return throwError(errorMessage);
-    }
   }
 
   /*
