4bd15fed7e0ab4d65a1adf07a38a48bfb9d9183b
Stefan <shodgeskluck@gmail.com>
2019-06-28 15:58:30 -0400

    Set up slide L/R animations between component views

--- src/app/animations.module.ts
+++ src/app/animations.module.ts
@@ -2,7 +2,31 @@ import { transition, trigger, query, style, animate, group, animateChild } from 
 
 export const slideInAnimation =
   trigger('routeAnimations', [
-    transition('Welcome <=> About', [
+    transition('* => Welcome', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ], { optional: true }),
+      query(':enter', [
+        style({ left: '-100%'})
+      ], { optional: true }),
+      query(':leave', animateChild(), { optional: true }),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({ left: '100%'}))
+        ], { optional: true }),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%'}))
+        ], { optional: true })
+      ]),
+      query(':enter', animateChild())
+    ]),
+    transition('* => *', [
       style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
@@ -11,19 +35,19 @@ export const slideInAnimation =
           right: 0,
           width: '100%'
         })
-      ]),
+      ], { optional: true }),
       query(':enter', [
         style({ right: '-100%'})
-      ]),
-      query(':leave', animateChild()),
+      ], { optional: true }),
+      query(':leave', animateChild(), { optional: true }),
       group([
         query(':leave', [
           animate('300ms ease-out', style({ right: '100%'}))
-        ]),
+        ], { optional: true }),
         query(':enter', [
           animate('300ms ease-out', style({ right: '0%'}))
-        ])
+        ], { optional: true })
       ]),
-      query(':enter', animateChild()),
+      query(':enter', animateChild(), { optional: true })
     ])
   ]);
