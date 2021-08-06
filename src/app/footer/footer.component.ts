2170d5f403b6cc5682b598e2d15a0610ce2c84ad
Stefan <shodgeskluck@gmail.com>
2021-08-05 20:55:21 -0400

    Updating footer date to be dynamic, plus npm update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,10 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
+  public currentYear: number;
 
   constructor() { }
 
   ngOnInit() {
+    let currentDate = new Date();
+    this.currentYear = currentDate.getFullYear();
   }
 
 }
